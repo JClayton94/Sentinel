309dc9783a7a1890906a66bc86b1695d1a341215
anita <anitachandlermohan@gmail.com>
2019-02-26 10:21:28 +0000

    keywords json file and js script to insert keywords in to mongodatabase

---
+++ keywordslist.js
@@ -0,0 +1,31 @@
+let mongoose = require('mongoose');
+let url = "mongodb://localhost:27017/sentineldatabase";
+
+mongoose.connect(url);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error:'));
+db.once('open', function(){
+    let keywordSchema = new mongoose.Schema({
+        word: String,
+       
+    })
+
+    let keywordModel = mongoose.model('keywords', keywordSchema);
+    let keywordlist = [{word: 'plum'},{word:'plumboy'},{word:'Sentinel'}, {word:'keyword'},{word:'tea'} ]
+
+    keywordModel.insertMany(keywordlist, function(err){
+        if (err) return console.error(err);
+        console.log('words in database!');
+    });
+    keywordModel.find(function (err, keywordlist){
+        if (err) return console.error(err);
+        console.log(keywordlist);
+    });
+    process.on('SIGINT',function(){
+        db.close(function (){
+            console.log('disconnected');
+            process.exit(0);
+        })
+    });
+
+});
