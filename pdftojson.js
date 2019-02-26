3ebaef90bbc85196b913934264e1e137bf866b10
JClayton94 <joshclaytonmarrs@gmail.com>
2019-02-26 09:56:55 +0000

    Finished converter, can change directories with config file

--- pdftojson.js
+++ pdftojson.js
@@ -2,22 +2,20 @@ const PDFParser = require("pdf2json");
 const fs = require("fs");
 
 let dir = JSON.parse(fs.readFileSync("./directoryConfig.json", "utf8"));
-let dirLength = fs.readdirSync(dir.pdfDir).length;
+let dirFiles = fs.readdirSync(dir.pdfDir);
+let dirLength = dirFiles.length;
 let pdf;
 
-console.log(dirLength);
-console.log(dir.jsonDir);
-console.log(dir.pdfDir);
+for(let i = 0; i < dirLength; i++){
 
-for(let i = 1; i < dirLength + 1; i++){
-    
     pdf  = new PDFParser();
     pdf.on("pdfParser_dataError", errData => console.error(errData.parserError));
     pdf.on("pdfParser_dataReady", pdfData => {
-            fs.writeFile(dir.jsonDir + i +  ".json", JSON.stringify(pdfData), err => {
+            
+            fs.writeFile(dir.jsonDir + "documentJSON_" + i +  ".json", JSON.stringify(pdfData), err => {
                 if (err) throw err;
             });
         }); 
         
-    pdf.loadPDF(dir.pdfDir + "test" + i + ".pdf");
+    pdf.loadPDF(dir.pdfDir + dirFiles[i]);
 }
