b6ccaabb58a48e6d5d2564b0f5155724cca498d5
JClayton94 <joshclaytonmarrs@gmail.com>
2019-02-26 09:34:58 +0000

    removed excess documents

--- jsonArray.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-let dir = JSON.parse(fs.readFileSync("./directoryConfig.json", "utf8"));
-let jsonData = fs.readFileSync("./testJSON.json");
-let translatedData = JSON.parse(jsonData);
-console.log(translatedData);
