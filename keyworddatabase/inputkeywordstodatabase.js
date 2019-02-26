0358699c5f04ec5583129c586464bc26355d5495
anita <anitachandlermohan@gmail.com>
2019-02-26 14:51:24 +0000

    can read words from text file and input into mongo database

---
+++ keyworddatabase/inputkeywordstodatabase.js
@@ -0,0 +1,41 @@
+const express = require('express');
+let mongoose = require('mongoose');
+let url = "mongodb://localhost:27017/sentineldatabase";
+let fs = require('fs');
+const app = express();
+const port = 2008;
+let keywordsarray;
+let keywordModel = require('./keywordmodelarray.js');
+
+fs.readFile('NSAkeywordlist.txt',function(err,data){
+    if (err) throw err;
+    keywordsarray = data.toString().split(",");
+    for(let i = 0;i < keywordsarray.length;i++){
+        console.log(keywordsarray[i]);
+
+    }
+    return keywordsarray;
+});
+
+app.listen(port,function(){
+console.log('listening on port');
+});
+mongoose.connect(url);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error:'));
+db.once('open', function(){
+    let wordstoinput = keywordModel({words:keywordsarray});
+    wordstoinput.save(function(err){
+        if(err) throw err;
+        console.log('input to database from textfile')
+    });
+    
+    process.on('SIGINT',function(){
+        db.close(function (){
+            console.log('disconnected');
+            process.exit(0);
+        })
+    });
+});
+
+
--- keyworddatabase/keywordmodelarray.js
+++ keyworddatabase/keywordmodelarray.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 let keywordSchema = new mongoose.Schema({
-        words: Array, 
+        words: [String], 
     })
 let keywordModel = mongoose.model('keywords', keywordSchema);
 module.exports = keywordModel;
