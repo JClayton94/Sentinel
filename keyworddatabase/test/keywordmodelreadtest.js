251e6bd4d71ff8abdcf37bb9862369420a5ed190
anita <anitachandlermohan@gmail.com>
2019-02-27 15:39:51 +0000

    unit tests for database CRUD functionality complete

--- keyworddatabase/test/keywordmodelreadtest.js
+++ keyworddatabase/test/keywordmodelreadtest.js
@@ -2,9 +2,10 @@ let expect = require('chai').expect;
 let keywordModel = require('../keywordmodelarray.js')
 let mongoose = require('mongoose');
 let assert = require('assert');
-let keywordTest;
+
 
 describe('hooks',function(){
+    let keywordTest;
 beforeEach(function(done){
         let keywordTest = new keywordModel({words:['plum']});
         keywordTest.save(done);
@@ -17,24 +18,55 @@ beforeEach(function(done){
 
                 
 
-describe('Read keywords',()=>{
+    describe('Read keywords',()=>{
 
-    it('find list with plum ',function(done){
-        keywordModel.findOne({words:'plum'},function(err,res){
-            if(err) return done(err);
-            assert(res.words).to.equal(['plum'])
-        })
-    })
-})
-describe('Creating keywordlist',()=>{
-    it('creates keyword list',(done)=>{
-        let keywordTest = new keywordModel({words:['key','words']});
-        keywordTest.save()
-            .then(()=>{
-                assert(!keywordTest.isNew);
+        it('find list with plum ',function(done){
+            keywordModel.findOne({words:'plum'},function(err,res){
+                if(err) return done(err);
+                // assert(res.words).equal(['plum'])
+                assert(res.words == 'plum');
                 done();
             })
-
+        })
     })
+    describe('Creating keywordlist',()=>{
+        it('creates keyword list',(done)=>{
+            let keywordTest = new keywordModel({words:['key','words']});
+            keywordTest.save()
+                .then(()=>{
+                    assert(!keywordTest.isNew);
+                    done();
+                })
+
+        })
+    })
+
+    describe('delete keywordlist',()=>{
+        it('remove keyword using instance',function(done){
+            
+            keywordModel.deleteOne({words:['plum']})
+            .then(()=> keywordModel.findOne({words:['plum']},function(err,res){
+                if(err) return done(err);
+                assert(res == null);
+                done();
+            }));
+        })
+    })
+
+    describe('update keywordslist',()=>{
+        it('update list with different words',function(done){
+            keywordModel.updateOne(
+                {words:['plum']},
+                {$set:{words:['grape']}}
+            )
+            .then(()=>keywordModel.findOne({words:['grape']},function(err,res){
+                if(err) return done(err);
+                assert(res.words == 'grape');
+                done();
+            }))
+        })
+    })
+
+                 
+
 })
-});
