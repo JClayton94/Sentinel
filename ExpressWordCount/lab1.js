f73b6c3cd5251139eb5592de14e253634321e771
Jakobrennan <jakob2011@hotmail.co.uk>
2019-02-28 11:41:13 +0000

    hopefully, this time

---
+++ ExpressWordCount/lab1.js
@@ -0,0 +1,55 @@
+const fileSys = require('fs');
+const _ = require('lodash');
+const assessedDocImport = fileSys.readFileSync('./jsons/content2.json', 'utf8');
+const keywordsImport = fileSys.readFileSync('keywords.json', 'utf8');
+const assessedDoc = JSON.parse(assessedDocImport);
+const assessedPages = assessedDoc.formImage.Pages;
+const assessedTexts = assessedDoc.formImage.Pages[0].Texts;
+const keywordList = JSON.parse(keywordsImport);
+const keywordCount = [];
+const extractedText = [];
+
+try {
+  // const keywordList = JSON.parse(keywordsImport);
+  for (var word in keywordList.keywords) {
+    // let newWord = { keywordList.keywords[word] : word};
+    // tempKeywords.push(newWord);
+    // console.log(keywordList.keywords[word]);
+  }
+} catch (e) {
+    console.log(e);
+}
+
+for (let text in assessedPages) {
+    let tempHold = assessedPages[text].Texts;
+    for (let word in tempHold) {
+      // console.log(tempHold[word].R[0].T);
+      let temp = tempHold[word].R[0].T.replace(new RegExp("%E2%80%99", "g"), "'")
+      .replace(new RegExp("%2C", "g"), " ")
+      .replace(new RegExp("%3A", "g"), ":")
+      .replace(/"%2F"/g, "/").split("%20");
+      for (var caught in temp) {
+        extractedText.push(temp[caught]);
+      }
+    }
+}
+
+for (let targetWord in keywordList.keywords) {
+  let counter = 0;
+  for (let caughtWord in extractedText) {
+    if (extractedText[caughtWord] == keywordList.keywords[targetWord]) {
+      counter++;
+    }
+  }
+  let tempObject = { word: keywordList.keywords[targetWord], count: counter };
+  keywordCount.push(tempObject);
+}
+
+console.log(keywordCount);
+
+fileSys.writeFile('extractedWords3.txt', extractedText, function (err) {
+  if (err) throw err;
+  // console.log("done");
+});
+
+// console.log(extractedText);
