14fef788100b5148172487c229f8c55d9cdbadc0
Chris-CCW <cyberfilth@protonmail.com>
2019-03-01 13:38:29 +0000

    Changed constant to uppercase

--- pdftojson.js
+++ pdftojson.js
@@ -1,20 +1,20 @@
-const PDFParser = require("pdf2json");
-const fs = require("fs");
+const PDFPARSER = require("pdf2json");
+const FS = require("fs");
 PDFConverter();
 
 function PDFConverter() {
-  let dir = JSON.parse(fs.readFileSync("./PDFConvertConfig.json", "utf8"));
-  let dirFiles = fs.readdirSync(dir.pdfDir);
+  let dir = JSON.parse(FS.readFileSync("./PDFConvertConfig.json", "utf8"));
+  let dirFiles = FS.readdirSync(dir.pdfDir);
   let dirLength = dirFiles.length;
   let jsonDocName = dir.jsonRaw;
   var jsonDocArray = [];
   let pdf;
 
   for (let i = 0; i < dirLength; i++) {
-    pdf = new PDFParser();
+    pdf = new PDFPARSER();
     pdf.on("pdfParser_dataError", errData => console.error("error" + errData.parserError));
     pdf.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile(dir.jsonDir + jsonDocName + i + ".json", JSON.stringify(pdfData), err => {
+    FS.writeFile(dir.jsonDir + jsonDocName + i + ".json", JSON.stringify(pdfData), err => {
     jsonDocArray.push(jsonDocName + i + ".json");
     if (err) throw err;
     });
