7f9df5c7c9a87e1392e51e5e96d0d10271647bb2
Chris-CCW <cyberfilth@protonmail.com>
2019-03-01 13:34:33 +0000

    Updated code to reflect coding standards

--- pdftojson.js
+++ pdftojson.js
@@ -2,26 +2,25 @@ const PDFParser = require("pdf2json");
 const fs = require("fs");
 PDFConverter();
 
-function PDFConverter(){
+function PDFConverter() {
+  let dir = JSON.parse(fs.readFileSync("./PDFConvertConfig.json", "utf8"));
+  let dirFiles = fs.readdirSync(dir.pdfDir);
+  let dirLength = dirFiles.length;
+  let jsonDocName = dir.jsonRaw;
+  var jsonDocArray = [];
+  let pdf;
 
-let dir = JSON.parse(fs.readFileSync("./PDFConvertConfig.json", "utf8"));
-let dirFiles = fs.readdirSync(dir.pdfDir);
-let dirLength = dirFiles.length;
-let jsonDocName = dir.jsonRaw;
-var jsonDocArray = [];
-let pdf;
-
-for(let i = 0; i < dirLength; i++){
-    pdf  = new PDFParser();
+  for (let i = 0; i < dirLength; i++) {
+    pdf = new PDFParser();
     pdf.on("pdfParser_dataError", errData => console.error("error" + errData.parserError));
-    pdf.on("pdfParser_dataReady", pdfData => {  
-            fs.writeFile(dir.jsonDir + jsonDocName + i +  ".json", JSON.stringify(pdfData), err => {
-                jsonDocArray.push(jsonDocName + i + ".json");      
-                if (err) throw err;
-            });
-        }); 
+    pdf.on("pdfParser_dataReady", pdfData => {
+    fs.writeFile(dir.jsonDir + jsonDocName + i + ".json", JSON.stringify(pdfData), err => {
+    jsonDocArray.push(jsonDocName + i + ".json");
+    if (err) throw err;
+    });
+    });
     pdf.loadPDF(dir.pdfDir + dirFiles[i]);
-    }
+  }
 }
 
 module.exports = PDFConverter;
